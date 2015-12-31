111ec9fa9b245e58c243bd97d1c019d3896dfa1e
bom_d_van <bom.d.van@gmail.com>
2015-12-31 10:48:31 +0800

    van-tail-mysql-log.sh: set log file

--- van-tail-mysql-log.sh
+++ van-tail-mysql-log.sh
@@ -8,6 +8,7 @@ function set_log_off() {
 	mysql -u root -e "SET GLOBAL general_log = 'OFF';"
 }
 
+mysql -u root -e "SET GLOBAL general_log_file = '/usr/local/var/mysql/Vans-MacBook-Pro.log';"
 mysql -u root -e "SET GLOBAL general_log = 'ON';"
 
 tail -f /usr/local/var/mysql/Vans-MacBook-Pro.log
